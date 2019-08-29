db0bdaae529b981de6919adc21e8894493994824
nirvin <noelle.r.irvin@gmail.com>
2019-08-28 20:08:31 -0400

    added header

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import enzyme from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App test', () => {
+
+  it('should render', () => {
+    const wrapper = enzyme.shallow(<App />);
+    expect(wrapper.find('.App')).toHaveLength(1);
+  });
+
+  it('renders header', () => {
+    const wrapper = enzyme.shallow(<App />);
+    expect(wrapper.find('header'))
+      .toContainEqual(<header>Mitch's Blog</header>);
+  });
+
 });

976635f6d6570abefed80574d34124e9f7142c32
mitchellirvin <mitchell.k.irvin@gmail.com>
2020-05-30 14:55:28 -0400

    making loading of posts parallelized, fixing some old tests

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { PostType } from './types/PostType';
 import Showdown from 'showdown';
 import './App.css';
 import axios from 'axios';
+import update from 'immutability-helper';
 
 interface AppState {
     posts: PostType[]
@@ -16,78 +17,82 @@ class App extends React.Component<{}, AppState> {
     constructor(props: {}) {
         super(props);
         this.state = {
-            posts: []
+            posts: [
+                {
+                    title: 'The Father\'s Guidance',
+                    subtitle: 'God is our Father. How, then, does he guide us as his children?', 
+                    date: new Date('May 30, 2020'),
+                    body: "",
+                    url: '/the-fathers-guidance'
+                },
+                {
+                    title: 'Truth in the Face of Suffering',
+                    subtitle: 'Faced with death, or any kind of related suffering, what is the Truth that our worldview (salvation by faith in the God of the Christian bible) offers us?', 
+                    date: new Date('May 9, 2020'),
+                    body: "",
+                    url: '/truth-in-the-face-of-suffering'
+                },
+                {
+                    title: 'Being Afraid',
+                    subtitle: 'An exploration off the first handful of counseling sessions Noelle and I shared.', 
+                    date: new Date('April 20, 2020'),
+                    body: "",
+                    url: '/being-afraid'
+                },
+                {
+                    title: 'The Investigator',
+                    subtitle: 'My initial response to, and processing of, Ian Cron\'s text "The Road Back to You". Specifically: chapter 9, "Type 5 - The Investigator".',
+                    date: new Date('Feb 24, 2020'),
+                    body: "",
+                    url: '/the-investigator'
+                },
+                {
+                    title: 'Welcome',
+                    subtitle: 'What exactly is "This Good Endeavor"?',
+                    date: new Date('Feb 20, 2020'),
+                    body: "",
+                    url: '/'
+                },
+                {
+                    title: 'What I Learned In My First Two Years as a Software Engineer',
+                    subtitle: 'Two stories, lessons learned, my regrets, and my goals after my first two years working as a software engineer.',
+                    date: new Date('Jan 17, 2019'),
+                    body: "",
+                    url: '/what-i-learned-in-my-first-two-years'
+                },
+                {
+                    title: 'Fundamental Money',
+                    subtitle: 'Navigating the transition from zero financial responsibility, to stewarding a steady income.',
+                    date: new Date('May 20, 2019'),
+                    body: "",
+                    url: '/fundamental-money'
+                }
+            ]
         };
         this.getPosts();
     }
 
-    private async getPosts() {
-        let posts: PostType[] = [
-            {
-                title: 'The Father\'s Guidance',
-                subtitle: 'God is our Father. How, then, does he guide us as his children?', 
-                date: new Date('May 30, 2020'),
-                body: this.markdownToHtmlConverter.makeHtml(
-                    await axios.get('./posts/the-fathers-guidance.md')
-                        .then(postBodiesResponse => postBodiesResponse.data)),
-                url: '/the-fathers-guidance'
-            },
-            {
-                title: 'Truth in the Face of Suffering',
-                subtitle: 'Faced with death, or any kind of related suffering, what is the Truth that our worldview (salvation by faith in the God of the Christian bible) offers us?', 
-                date: new Date('May 9, 2020'),
-                body: this.markdownToHtmlConverter.makeHtml(
-                    await axios.get('./posts/truth-in-the-face-of-suffering.md')
-                        .then(postBodiesResponse => postBodiesResponse.data)),
-                url: '/truth-in-the-face-of-suffering'
-            },
-            {
-                title: 'Being Afraid',
-                subtitle: 'An exploration off the first handful of counseling sessions Noelle and I shared.', 
-                date: new Date('April 20, 2020'),
-                body: this.markdownToHtmlConverter.makeHtml(
-                    await axios.get('./posts/being-afraid.md')
-                        .then(postBodiesResponse => postBodiesResponse.data)),
-                url: '/being-afraid'
-            },
-            {
-                title: 'The Investigator',
-                subtitle: 'My initial response to, and processing of, Ian Cron\'s text "The Road Back to You". Specifically: chapter 9, "Type 5 - The Investigator".',
-                date: new Date('Feb 24, 2020'),
-                body: this.markdownToHtmlConverter.makeHtml(
-                    await axios.get('./posts/the-investigator.md')
-                        .then(postBodiesResponse => postBodiesResponse.data)),
-                url: '/the-investigator'
-            },
-            {
-                title: 'Welcome',
-                subtitle: 'What exactly is "This Good Endeavor"?',
-                date: new Date('Feb 20, 2020'),
-                body: this.markdownToHtmlConverter.makeHtml(
-                    await axios.get('./posts/welcome.md')
-                        .then(postBodiesResponse => postBodiesResponse.data)),
-                url: '/'
-            },
-            {
-                title: 'What I Learned In My First Two Years as a Software Engineer',
-                subtitle: 'Two stories, lessons learned, my regrets, and my goals after my first two years working as a software engineer.',
-                date: new Date('Jan 17, 2019'),
-                body: this.markdownToHtmlConverter.makeHtml(
-                    await axios.get('./posts/what-i-learned-in-my-first-two-years.md')
-                        .then(postBodiesResponse => postBodiesResponse.data)),
-                url: '/what-i-learned-in-my-first-two-years'
-            },
-            {
-                title: 'Fundamental Money',
-                subtitle: 'Navigating the transition from zero financial responsibility, to stewarding a steady income.',
-                date: new Date('May 20, 2019'),
-                body: this.markdownToHtmlConverter.makeHtml(
-                    await axios.get('./posts/fundamental-money.md')
-                        .then(postBodiesResponse => postBodiesResponse.data)),
-                url: '/fundamental-money'
-            }
-        ];
-        this.setState({posts});
+    private getPosts() {
+        this.state.posts.forEach((post, index) => {
+            const postFileName = this.getPostFileName(post);
+            axios.get(postFileName)
+                .then(postBodiesResponse => {
+                    post.body = this.markdownToHtmlConverter.makeHtml(postBodiesResponse.data);
+                    this.setState(update(
+                        this.state, 
+                        { posts: { [index]: { $set: post } } }
+                    ));
+                });
+        });
+    }
+
+    private getPostFileName(post: PostType): string {
+        const prefix = "./posts";
+        const suffix = ".md";
+        if (post.url === "/") {
+            return prefix + "/welcome" + suffix;
+        }
+        return prefix + post.url + suffix;
     }
 
     render() {
--- src/blog/posts/Posts.test.tsx
+++ src/blog/posts/Posts.test.tsx
@@ -8,26 +8,27 @@ describe('Posts test', () => {
     const posts = [
         {
             title: 'Title1',
+            subtitle: 'subtitle1',
             date: new Date('December 17, 1995'),
-            body: 'Lorem ipsum...'
+            body: 'Lorem ipsum...',
+            url: '/title1',
         },
         {
             title: 'Title2',
+            subtitle: 'subtitle2',
             date: new Date('March 12, 1995'),
-            body: 'Best day...'
+            body: 'Lorem ipsum...',
+            url: '/title2',
         }
     ];
 
     it('should render title and posts', () => {
         const wrapper = enzyme.shallow(<Posts posts={posts} />);
-        expect(wrapper.find('.title').at(0).text())
-            .toEqual('Posts');
         expect(wrapper.find(Post)).toHaveLength(2);
         expect(wrapper.find(Post).at(0).prop('post'))
             .toEqual(posts[0]);
         expect(wrapper.find(Post).at(1).prop('post'))
             .toEqual(posts[1]);
-
     });
 
 });

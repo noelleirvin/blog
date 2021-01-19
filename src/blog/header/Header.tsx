9d25b64919971784bcd50a0f4ea522c219a08d2a
noelleirvin <noelle.r.irvin@gmail.com>
2021-01-18 20:19:47 -0500

    removed unused logo

--- src/blog/header/Header.tsx
+++ src/blog/header/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
-import logo from './blog-logo.png';
 
 class Header extends React.Component<{}, {}> {
     render() {

021dc066889b030e2a1104b6bacabfffd55d9f27
noelleirvin <noelle.r.irvin@gmail.com>
2021-01-27 19:29:25 -0500

    posts and responsive styling

--- src/blog/read/Read.tsx
+++ src/blog/read/Read.tsx
@@ -11,11 +11,13 @@ interface Props {
 function Read (props: Props) {
     return (
         <div className='read'>
-            <div className='heading'>
-                <h1 className='title'>{props.post.title}</h1>
-                <p className='date'>{moment(props.post.date).format('MMMM Do YYYY')}</p>
-            </div>
-            <p className='subtitle'>{props.post.subtitle}</p>
+            <span className='mobileHeading'>
+                <div className='heading'>
+                    <p className='date'>{moment(props.post.date).format('MMMM Do YYYY')}</p>
+                    <h1 className='title'>{props.post.title}</h1>
+                </div>
+                <p className='subtitle'>{props.post.subtitle}</p>
+            </span>
             <div className='body'>{parse(props.post.body)}</div>
         </div>
     );
